06000b708c57aae96c4a8c2d41a5543ad0ae0716
ShubhamPipaliya <120780002+Madworld012@users.noreply.github.com>
2023-01-02 18:15:50 +0530

    -- add bet data in every palce bet event

--- classes/cashClass.class.js
+++ classes/cashClass.class.js
@@ -18,15 +18,15 @@ module.exports = {
                     }
                     db.collection('withdrawal_request').insertOne(withdrawal_data);
                     commonClass.update_cash({ uid: user_data._id.toString(), cash: -data.amount, msg: "Withdrawal Request" });
-                    commonClass.sendDirectToUserSocket(client, { en: "WITHDRAWAL", data: { status: true, msg: "Withdrawal Request Added, Admin Can Contact you soon !" } });
+                    commonClass.sendDirectToUserSocket(client, { en: "WITHDRAWAL", data: { status: true, cash: -data.amount, msg: "Withdrawal Request Added, Admin Can Contact you soon !" } });
                 } else {
-                    commonClass.sendDirectToUserSocket(client, { en: "WITHDRAWAL", data: { status: false, msg: "Not Enought Cash In YOur Account !" } });
+                    commonClass.sendDirectToUserSocket(client, { en: "WITHDRAWAL", data: { status: false, cash: -data.amount, msg: "Not Enought Cash In YOur Account !" } });
                 }
             } else {
-                commonClass.sendDirectToUserSocket(client, { en: "WITHDRAWAL", data: { status: false, msg: "User Not Found" } });
+                commonClass.sendDirectToUserSocket(client, { en: "WITHDRAWAL", data: { status: false, cash: -data.amount, msg: "User Not Found" } });
             }
         } else {
-            commonClass.sendDirectToUserSocket(client, { en: "WITHDRAWAL", data: { status: false, msg: "Please Send Proper Data" } });
+            commonClass.sendDirectToUserSocket(client, { en: "WITHDRAWAL", data: { status: false, cash: -data.amount, msg: "Please Send Proper Data" } });
         }
     }
 }

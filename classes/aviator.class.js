eed675c1ea37404c31b799d87ded4ba05b2dc516
ShubhamPipaliya <120780002+Madworld012@users.noreply.github.com>
2023-02-15 16:59:52 +0530

    - update time.

--- classes/aviator.class.js
+++ classes/aviator.class.js
@@ -217,7 +217,7 @@ module.exports = {
             } else {
 
                 // await sleep((x > 20) ? 5 : 110 / x);
-                await sleep((x > 20) ? 5 : 500 / x + 4);
+                await sleep((x > 20) ? 5 : 200 / x + 1);
                 x = parseFloat((x + 0.01).toFixed(2));
                 aviatorClass.autoCutUser(tblid.toString(), x);
                 commonClass.sendToRoom(tblid.toString(), { en: "FLAY", data: { x: x } });

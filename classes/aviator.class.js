aca11eb2b10ac8fc30e83580cf24b8a8a33f41e5
ShubhamPipaliya <120780002+Madworld012@users.noreply.github.com>
2023-02-20 17:25:07 +0530

    -- update speed .

--- classes/aviator.class.js
+++ classes/aviator.class.js
@@ -227,8 +227,10 @@ module.exports = {
 
                 if (x > 50 && x <= 100) {
                     ix = (cut_out_x_value < x + 0.02) ? 0.01 : 0.02;
-                } else if (x > 100) {
+                } else if (x > 100 && x <= 200) {
                     ix = (cut_out_x_value < x + 0.03) ? 0.01 : 0.03;
+                } else if (x > 200) {
+                    ix = (cut_out_x_value < x + 0.07) ? 0.01 : 0.07;
                 }
 
                 x = parseFloat((x + ix).toFixed(2));
